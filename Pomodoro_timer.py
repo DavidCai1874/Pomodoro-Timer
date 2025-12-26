13002ee2449178c1b0469ea39477953a25dd72fb
David Cai <ycai53318@ecuad.ca>
2025-12-25 18:28:42 -0800

    Clean up files and update readme

---
+++ Pomodoro_timer.py
@@ -0,0 +1,87 @@
+#EN_comment_version
+
+import time
+import tkinter as tk
+from tkinter import messagebox
+
+class PomodoroTimer:
+    def __init__(self, root):
+        self.root = root
+        self.root.title("Pomodoro")
+        self.root.geometry("250x130")
+        
+        #background color
+        self.root.config(bg="#FAF7F0")
+        
+        #counting down number
+        self.timer_text = tk.StringVar()
+        self.timer_label = tk.Label(
+            root, 
+            textvariable=self.timer_text, 
+            font=("Comic Sans MS", 24, "bold"), # font
+            fg="#4A4947", #text color
+            bg="#FAF7F0" #background color
+        )
+        
+        #button to start next round
+        self.start_button = tk.Button(
+            root, 
+            text="Start Next Round", 
+            command=self.start_timer, 
+            font=("Comic Sans MS", 12, "bold"), #font
+            fg="#CE5A67", #text color
+            bg="#D8D2C2", #bg color
+            activebackground="#B17457" #color when button is clicked
+        )
+        
+        #How many rounds of timer u completed
+        self.count_label = tk.Label(
+            root, 
+            text="Rounds Completed: 0", 
+            font=("Comic Sans MS", 14),
+            fg="#4A4947",
+            bg="#FAF7F0"
+        )
+        
+        #initialize the timer
+        self.session_count = 0
+        self.running = False
+        self.timer_text.set("25:00")
+        self.start_timer()
+
+    def countdown(self, t):
+        self.timer_label.pack(pady=30)  #show counting
+        self.start_button.pack_forget()  #hide button
+        self.count_label.pack_forget()  #hide rounds
+        
+        if t >= 0 and self.running:
+            mins, secs = divmod(t, 60)
+            self.timer_text.set(f"{mins:02d}:{secs:02d}")
+            self.root.after(1000, self.countdown, t - 1) 
+        elif self.running:  #when countdown to 0
+            self.session_count += 1
+            self.count_label.config(text=f"Rounds Completed: {self.session_count}")
+            self.timer_label.pack_forget() #hide counting down
+            self.count_label.pack(pady=18) #show rounds, pady is the margin
+            self.start_button.pack(pady=0) #show button
+            self.root.attributes("-topmost", True) #make sure window is at top-most
+            
+            #after 4 rounds
+            if self.session_count % 4 == 0:
+                messagebox.showinfo("Long break Reminder", "You've completed four rounds. Take a 30-minute break!")
+                self.root.quit()
+            else:
+                messagebox.showinfo("Pomodoro Reminder", "Time's up! Take a break.")
+                self.running = False
+
+    def start_timer(self):
+        if not self.running:
+            self.running = True
+            self.start_button.pack_forget()
+            self.countdown(25 * 60)  # length of each round
+            self.root.attributes("-topmost", False)
+
+# creat window, instancing the class into the window
+root = tk.Tk()
+pomodoro = PomodoroTimer(root)
+root.mainloop()
--- README.md
+++ README.md
@@ -1,29 +1,35 @@
 # Pomodoro-Timer
-A Pomodoro Timer Program/ 一个番茄钟小程序
 
-中文版可以移步这里/ Here's the instructions in Chinese:[点击这里查看中文文档](README_CN.md)
+A Pomodoro Timer Program
 
-***
+## Features 
 
-# How to Use
-1. Download the file ending with **EN.exe**  
+- It doesn't use a countdown method that stops the program for each second, so **there will be no lag**
+
+- Each round has **25** minutes of focus time, and the program will be closed after **4** rounds. You can take a **15-30** minute break at this point
+
+- The break reminder will be **on top**, that is, you won’t miss it even if you're immersed in work.
+
+- After the reminder and before the next round, the window will also **stay on top**, so if it is not minimized, it will basically not be accidentally covered by other windows and forgotten to start the next round
+
+## Screenshots
+
+- ![](./readme_images/1.png)
+
+- ![](./readme_images/2.png)
+
+- ![](./readme_images/3.png)
+
+## Quick Start
+
+1. Download **Pomodoro_timer.exe**
+
 2. Double-click to start using it  
 
 **P.S.** If you're concerned about security, you can run it in Windows Sandbox for the first two hours, to verify its safety before using it on your desktop.  
 
-**P.S.S.** Mac users can download the `EN.py` file and try converting it into a Mac program themselves.  
+**P.S.S.** Mac users can download the `Pomodoro_timer.py` file and try converting it into a Mac program themselves.  
 
-# Pomodoro Timer
-- It doesn't use a countdown method that stops the program for each second, so **there will be no lag**
-- Each round has **25** minutes of focus time, and the program will be closed after **4** rounds. You can take a **15-30** minute break at this point
-- The break reminder will be **on top**, that is, you won’t miss it even if you're immersed in work.
-- After the reminder and before the next round, the window will also **stay on top**, so if it is not minimized, it will basically not be accidentally covered by other windows and forgotten to start the next round
+## Demo Video (YouTube)
 
-# Screenshots
-- ![en_timer](https://github.com/user-attachments/assets/ef7ac5e5-d90f-4358-aaa8-0f7bd7d91206)
-- ![en_break](https://github.com/user-attachments/assets/b7ebb7bb-b4a7-4b21-a4d8-08849ff1a51f)
-- ![en_round4](https://github.com/user-attachments/assets/ebda45e0-fd85-4237-89ed-9305c60bbb20)
-
-
-# Demo Video (YouTube)
 [![Watch on YouTube](https://img.youtube.com/vi/vfVE9N-U1Ss/0.jpg)](https://www.youtube.com/watch?v=vfVE9N-U1Ss)  
